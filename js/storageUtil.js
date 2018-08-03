e8aa5837b0f0abcab49d68f2bcb025a28df1dab8
markhuang1994 <markhuang1994@gmail.com>
2018-08-03 12:10:58 +0800

    -新增sync storage方法，使舊版本使用sync儲存的用戶能無縫接軌

--- js/storageUtil.js
+++ js/storageUtil.js
@@ -8,7 +8,21 @@
     async function getStorageData(key) {
         return new Promise(res => {
             chrome.storage.local.get(key, function (items) {
-                console.log(`%cget ${key} form chrome storage: `, 'color:#b90000;');
+                if (Object.keys(items).length > 0) {
+                    console.log(`%cget ${key} form chrome storage: `, 'color:#b90000;');
+                    console.log(JSON.stringify(items));
+                    res(items);
+                } else {
+                    getSyncStorageData(key).then(() => res());
+                }
+            });
+        })
+    }
+
+    async function getSyncStorageData(key) {
+        return new Promise(res => {
+            chrome.storage.sync.get(key, function (items) {
+                console.log(`%cget ${key} form chrome sync storage: `, 'color:#b90000;');
                 console.log(JSON.stringify(items));
                 res(items);
             });
